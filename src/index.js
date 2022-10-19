b5df25b818df614269e2ce9570a9e26ab78e52f8
Edozie <edoziechukwuma@gmail.com>
2022-10-18 18:12:54 -0600

    Fix linters

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './style.css';
 
 const display = (array) => {
-  const main = document.querySelector(".main");
-  main.innerHTML = "";
+  const main = document.querySelector('.main');
+  main.innerHTML = '';
   array.forEach((item) => {
     main.innerHTML += `<div class="box">
     <img class='image' src="${item.keyImages[0].url}" alt="">
@@ -16,17 +16,17 @@ const display = (array) => {
 };
 
 const options = async () => {
-  const response = await fetch("https://free-epic-games.p.rapidapi.com/free", {
-    method: "GET",
+  const response = await fetch('https://free-epic-games.p.rapidapi.com/free', {
+    method: 'GET',
     headers: {
-      "X-RapidAPI-Key": "42398f1d19msh93087ecbad4588bp1bf92ejsnd0ca092a3aa3",
-      "X-RapidAPI-Host": "free-epic-games.p.rapidapi.com",
+      'X-RapidAPI-Key': '42398f1d19msh93087ecbad4588bp1bf92ejsnd0ca092a3aa3',
+      'X-RapidAPI-Host': 'free-epic-games.p.rapidapi.com',
     },
   });
   const results = response.json();
   results.then((object) => display(object.freeGames.current));
 };
 
-window.addEventListener("load", () => {
+window.addEventListener('load', () => {
   options();
 });
